bba32ecdf7e24ca33eb3ce4864d4e8f8e529e1ec
EmreKaya0860 <h.emrekaya2207@gmail.com>
2022-10-26 21:50:05 +0300

    MVC Ödev

---
+++ HuseyinEmreKaya/Controllers/ProductsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HuseyinEmreKaya.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Televizyon()
+        {
+            return View();
+        }
+    }
+}
